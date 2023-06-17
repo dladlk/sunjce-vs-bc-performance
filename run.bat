ebbba0d95e997f344b956fd68b07cd92dcab6284
Dmytro Lapko <dlk@mercell.com>
2023-06-17 14:29:22 +0300

    fix: update jar name in run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 @SET MAX_SIZE_MB=5
+@SET JAR_PATH=target\sunjce-vs-bc-performance-1.0-shaded.jar
 
 @SET JAVA_PATH="C:\Program Files\Java\jdk8u372-b07\bin\java.exe"
-@%JAVA_PATH% -jar target\jca-performance-comp-1.0.0-shaded.jar %MAX_SIZE_MB%
+@%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
 
 @SET JAVA_PATH="C:\Program Files\Java\jdk-12\bin\java.exe"
-@%JAVA_PATH% -jar target\jca-performance-comp-1.0.0-shaded.jar %MAX_SIZE_MB%
+@%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
 
 @SET JAVA_PATH="C:\Program Files\Java\jdk-17\bin\java.exe"
-@%JAVA_PATH% -jar target\jca-performance-comp-1.0.0-shaded.jar %MAX_SIZE_MB%
+@%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
