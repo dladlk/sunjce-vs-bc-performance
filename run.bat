226d3e65f00ce0907c33fefca317cb9d14b58bb5
Dmytro Lapko <dlk@mercell.com>
2023-10-07 10:56:23 +0300

    feat: remove java 17 and java 20, switch to java 21

--- run.bat
+++ run.bat
@@ -1,14 +1,15 @@
 @SET MAX_SIZE_MB=5
-@SET JAR_PATH=target\sunjce-vs-bc-performance-1.0-shaded.jar
+@SET JAR_PATH=target\sunjce-vs-bc-performance-1.0.jar
+@REM goto j21
 
 @SET JAVA_PATH="C:\Program Files\Java\jdk8u372-b07\bin\java.exe"
 @%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
 
+:j12
 @SET JAVA_PATH="C:\Program Files\Java\jdk-12\bin\java.exe"
 @%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
 
-@SET JAVA_PATH="C:\Program Files\Java\jdk-17\bin\java.exe"
-@%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
+:j21
 
-@SET JAVA_PATH="C:\Program Files\Java\jdk-20.0.1+9\bin\java.exe"
-@%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
+@SET JAVA_PATH="C:\Program Files\Java\jdk-21\bin\java.exe"
+%JAVA_PATH% -jar %JAR_PATH% %MAX_SIZE_MB%
